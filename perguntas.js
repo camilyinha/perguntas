ce11b6a17ebffe1304a7a815a06b94accdd4d1ad
camilyinha <camilymoreiral123@gmail.com>
2024-11-25 13:55:54 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Quanto é 6x6',
+    '36'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Havaí',
+    'A capital é Honolulu'
+)
+
+criaCartao(
+    'História',
+    'Em que ano foi abolida a escravidão',
+    'Em 1888'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz "qual o seu nome" em inglês',
+    'What is your name'
+)
